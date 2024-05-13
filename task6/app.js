467d9a6e3ececfce09cfac72f64ba2c209403d04
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-13 02:21:10 -0700

    completed

---
+++ task6/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// Assign a function expression to a variable
+let function1 = function (value) {
+    console.log(value);
+};
+// Pass an argument into  function.
+function1(100);
+// Create normal function
+function function1Function(value) {
+    console.log(value);
+}
+// Passing an argument into  function.
+function1Function(200);
---
+++ task6/app.ts
@@ -0,0 +1,15 @@
+// Assign a function expression to a variable
+let function1 = function(value:number) {
+    console.log(value);
+};
+
+// Pass an argument into  function.
+function1(100);
+
+// Create normal function
+function function1Function(value: any) {
+    console.log(value);
+}
+
+// Passing an argument into  function.
+function1Function(200);
