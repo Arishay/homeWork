6de897a0b02fd93b8fc97c04c98b1c49648f499b
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-13 02:19:23 -0700

    completed

---
+++ task1/app.js
@@ -0,0 +1,5 @@
+//Task1
+var miles = 3;
+var oneKm = 1.60934;
+var kilometers = miles * oneKm;
+console.log("3 miles are equal to ".concat(kilometers, "Km"));
---
+++ task1/app.ts
@@ -0,0 +1,5 @@
+//Task1
+let miles = 3;
+let oneKm = 1.60934;
+let kilometers = miles*oneKm
+console.log(`3 miles are equal to ${kilometers}Km`); 
