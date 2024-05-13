73df7d2de47e38c4ef2cd17a24d9aed0eb01793c
Arisha Ghaffar <160336030+Arishay@users.noreply.github.com>
2024-05-13 02:20:15 -0700

    completed

---
+++ task4/app.js
@@ -0,0 +1,15 @@
+"use strict";
+let num1 = 6;
+let num2 = 4;
+function number(num1, num2) {
+    return num1 + num2;
+}
+let response = number(6, 4);
+console.log(response);
+let num3 = 19;
+let num4 = 4;
+function number1(num3, num4) {
+    return num3 + num4;
+}
+let call = number(19, 4);
+console.log(call);
---
+++ task4/app.ts
@@ -0,0 +1,16 @@
+let num1 = 6;
+let num2 = 4;
+
+function number(num1:number , num2:number) {
+    return num1 + num2
+}
+let response = number(6,4);
+console.log(response);
+
+let num3 = 19;
+let num4 = 4;
+function number1(num3:number, num4:number) {
+    return num3 + num4
+}
+let call = number(19,4)
+console.log(call);
